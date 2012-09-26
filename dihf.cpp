3d622d4ebe80f567fa117f752bd9b73a6d20923f
Jack Deeth <jackdeeth@gmail.com>
2012-09-26 14:01:42 +1000

    Corrected dihf.cpp to return degrees not radians

--- dihf.cpp
+++ dihf.cpp
@@ -31,22 +31,9 @@
 /*                                                                          */
 /****************************************************************************/
 
-struct pointComponents {
-  double x, y, z; //north, east, and downward component of magnetic field at
-  //a [geocentric/geodetic?] point. Units are nT.
-};
-
-struct pointField {
-  double d; //declination from geographic north (deg)
-  double i; //inclination (deg)
-  double h; //horizontal field strength
-  double f; //total field strength
-};
-
 pointField dihf (const pointComponents &xyz)
 {
-  int j;
-  double sn; //small number
+  double sn = 0.0001; //small number
   double h2;
   double hpx;
 
@@ -55,8 +42,6 @@ pointField dihf (const pointComponents &xyz)
   double z = xyz.z;
 
   double d, i, h, f;
-
-  sn = 0.0001;
   
   h2 = x*x + y*y;
   h = sqrt(h2);       /* calculate horizontal intensity */
@@ -88,8 +73,8 @@ pointField dihf (const pointComponents &xyz)
     }
 
   pointField dihf;
-  dihf.d = d;
-  dihf.i = i;
+  dihf.d = d * RAD2DEG;
+  dihf.i = i * RAD2DEG;
   dihf.h = h;
   dihf.f = f;
   return(dihf);
@@ -99,7 +84,7 @@ pointField dihf (const pointComponents &xyz)
 using namespace std;
 int main () {
   pointComponents input;
-  input.x = 27547.2;
+  input.x = 27547.2; //field components for 0N, 0E, 0ft, 2012.0
   input.y = -2817.5;
   input.z = -15628.3;
   cout <<   "Input x, y, z:" << endl <<
@@ -109,47 +94,8 @@ int main () {
 
   pointField output = dihf(input);
 
-  //copypasta
-  int   ddeg,ideg;
-  double dmin,imin;
-  double d, i;
-  d = output.d;
-  i = output.i;
-
-  /* Change d and i to deg and min */
-  ddeg=(int)d;
-  dmin=(d-(double)ddeg)*60;
-  if (d > 0 && dmin >= 59.5)
-    {
-      dmin -= 60.0;
-      ddeg++;
-    }
-  if (d < 0 && dmin <= -59.5)
-    {
-      dmin += 60.0;
-      ddeg--;
-    }
-
-  if (ddeg!=0) dmin=fabs(dmin);
-  ideg=(int)i;
-  imin=(i-(double)ideg)*60;
-  if (i > 0 && imin >= 59.5)
-    {
-      imin -= 60.0;
-      ideg++;
-    }
-  if (i < 0 && imin <= -59.5)
-    {
-      imin += 60.0;
-      ideg--;
-    }
-  if (ideg!=0) imin=fabs(imin);
-
-  //copypasta end
-
   cout << "Output d, i, h, f:" << endl <<
-          ddeg << "\'" << dmin << " " <<
-          ideg << "\'" << imin << " " <<
+          output.d << " " << output.i<< " " <<
           output.h << " " << output.f << endl;
 
   return 0;
